79b668682587d48232a3a4a7ad871d27386a1996
Koichi Murase <myoga.murase@gmail.com>
2013-11-08 04:44:49 +0900

    aghfonts 2013-11-08

--- uwsc/rate_to_p2.cpp
+++ uwsc/rate_to_p2.cpp
@@ -1,16 +1,20 @@
 #include <cstdio>
+#include <cstdlib>
 #include <cmath>
 
-int main(){
+int main(int argc,char** argv){
   // double const target=0.55586;
   // double const target=0.41684035014589412255106294289287;
-  double const target=0.33344448149383127709236412137379;
+  double target=0.33344448149383127709236412137379;
+  if(1<argc)
+    target=atof(argv[1]);
+
   double minError=10.0;
   int r1=100,r2=100;
 
   for(int i=1;i<100;i++){
     int j=int(0.5+target*1e4/i);
-    if(j>500)continue;
+    if(j>200)continue;
 
     double error=std::abs(target-i*j*1e-4);
     if(error<minError||error==minError&&i+j<r1+r2){
@@ -20,6 +24,7 @@ int main(){
     }
   }
 
+  std::printf("target = %.10g\n",target);
   std::printf("result : %dx%d (err = %fe+4)\n",r1,r2,minError*1e4);
   return 0;
 }
